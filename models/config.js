08619aa7c6bfe79bd197fbf88e4994e0b9f6bcf7
Robert Czikkel <robertczikkel@Roberts-MacBook-Pro.local>
2021-09-24 21:15:02 +0100

    conn string fixed

--- models/config.js
+++ models/config.js
@@ -1,10 +1,14 @@
 
 const Pool  = require('pg-pool');
-const process = require('process')
-const path = require('path')
 require('dotenv').config()
  
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const pool = new Pool({
+	connectionString:  connectionString, //Heroku addon will provide with a string called DATABASE_URL
+});
 // const config = {
 //     user: process.env.DB_ADMIN,
 //     host: process.env.DB_HOST,
@@ -13,14 +17,14 @@ require('dotenv').config()
 //     port: process.env.DB_PORT,
 // }
 
-const config = {
-    user: 'tdszxbhxyqxvyn',
-    host: 'ec2-54-145-110-118.compute-1.amazonaws.com',
-    database: 'dborv60r1692cf',
-    password: '949af96b7e7ed3d62fa9fa11f849e2d36efc7685bdcce963a92de93a05f8b4cc',
-    port: 5432,
-}
-const pool = new Pool(config)
+// const config = {
+//     user: 'tdszxbhxyqxvyn',
+//     host: 'ec2-54-145-110-118.compute-1.amazonaws.com',
+//     database: 'dborv60r1692cf',
+//     password: '949af96b7e7ed3d62fa9fa11f849e2d36efc7685bdcce963a92de93a05f8b4cc',
+//     port: 5432,
+// }
+// const pool = new Pool(config)
 
 module.exports = {
     pool
